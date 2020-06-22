cd395a9bbb95d9bb2c9fdf154d30f5e62704acd1
Naomi Talla <47737897+naamiix@users.noreply.github.com>
2020-06-22 19:07:41 +0200

    production et difussion page design done

--- diffusion-production.php
+++ diffusion-production.php
@@ -17,14 +17,8 @@
     <link href="./styles/diffusion-production.css" rel="stylesheet">
 </head>
 <body>
-    <div class="production-diffusion">
-        <div class="production">
-            <a href="#"><img src="./images/map.jpg" alt="Voir la map"></a>
-        </div>
-        <div class="diffusion">
-            <a href="diffusion.php"><img src="./images/saisons.jpg" alt="Voir les saisons"></a>
-        </div>
-    </div>
+    <a href="diffusion.php"><img class="diffusion-page" src="./images/saisons.png"></a>
+    <a href="#"><img class="country-map" src="./images/country-map.png"></a>
     <a href="index.php"><img src="./images/FLECHEGAUCHEJAUNE.png" alt="Ecran précédent" class="return"></a>
 </body>
 </html>
